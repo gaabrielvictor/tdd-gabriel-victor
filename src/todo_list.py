c735597095809edbb3e41885a61f852e9a474b0d
gaabrielvictor <gabrielflavictor@gmail.com>
2025-11-09 12:09:42 -0300

    feat: implementa método remover_tarefa

--- src/todo_list.py
+++ src/todo_list.py
@@ -1,16 +1,18 @@
 class ToDoList:
     def __init__(self):
-        self.tarefas = []
+        self._tarefas = []
 
     def adicionar_tarefa(self, titulo, descricao):
-        tarefa = {"titulo": titulo, "descricao": descricao, "concluida": False}
-        self.tarefas.append(tarefa)
+        self._tarefas.append({"titulo": titulo, "descricao": descricao, "concluida": False})
 
     def listar_tarefas(self):
-        return self.tarefas
+        return list(self._tarefas)
 
     def concluir_tarefa(self, titulo):
-        for tarefa in self.tarefas:
+        for tarefa in self._tarefas:
             if tarefa["titulo"] == titulo:
                 tarefa["concluida"] = True
                 break
+
+    def remover_tarefa(self, titulo):
+        self._tarefas = [t for t in self._tarefas if t["titulo"] != titulo]
