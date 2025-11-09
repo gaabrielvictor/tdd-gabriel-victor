6bdc09401559963ed9931be19cf64ab6bc722dff
gaabrielvictor <gabrielflavictor@gmail.com>
2025-11-09 11:47:17 -0300

    implementa método adicionar_tarefa e listar_tarefas

--- src/todo_list.py
+++ src/todo_list.py
@@ -1,2 +1,10 @@
 class ToDoList:
-    pass
+    def __init__(self):
+        self.tarefas = []
+
+    def adicionar_tarefa(self, titulo, descricao):
+        tarefa = {"titulo": titulo, "descricao": descricao, "concluida": False}
+        self.tarefas.append(tarefa)
+
+    def listar_tarefas(self):
+        return self.tarefas
