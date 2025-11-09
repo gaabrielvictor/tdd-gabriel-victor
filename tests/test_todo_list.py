cbf390b1c04505f13dda4be4c09eb7afafbfaf81
gaabrielvictor <gabrielflavictor@gmail.com>
2025-11-09 12:08:36 -0300

    test: cria teste falho para remover tarefa

--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -4,10 +4,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.todo_list import ToDoList
 
 
-def test_concluir_tarefa():
+def test_remover_tarefa():
     todo = ToDoList()
-    todo.adicionar_tarefa("Estudar TDD", "Ler sobre o ciclo Red-Green-Refactor")
-    todo.concluir_tarefa("Estudar TDD")
+    todo.adicionar_tarefa("Ler livro", "Clean Code")
+    todo.remover_tarefa("Ler livro")
     tarefas = todo.listar_tarefas()
 
-    assert tarefas[0]["concluida"] is True
+    assert len(tarefas) == 0
